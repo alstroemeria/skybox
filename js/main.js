a6b933f38ea6059fe5097d1395f7bf6eaa40b5b2
alstroemeria <alstroemeri@gmail.com>
2014-05-26 01:58:48 -0400

    Fixed Overflow, Renamed depedencies

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+
+        document.getElementById("page-top").style.overflowY = "visible";
+    });
+});
+
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(-1);
+});
