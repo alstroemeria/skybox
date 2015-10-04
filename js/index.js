42011c49ad7c79b75cca68bd55aa3e1b5567057b
alstroemeria <alstroemeri@gmail.com>
2015-10-04 18:59:10 -0400

    Fancy animations

--- js/index.js
+++ js/index.js
@@ -4,19 +4,21 @@ var texture_placeholder,
 isUserInteracting = false,
 onMouseDownMouseX = 0, onMouseDownMouseY = 0,
 vlon = 0.1, vlat =0, dY=0,dX=0
-lon = 90, onMouseDownLon = 0,
-lat = 0, onMouseDownLat = 0,
+lon = 200, onMouseDownLon = 0,
+lat = 8, onMouseDownLat = 0,
 phi = 0, theta = 0, mid_y = 0, mid_x= 0,
 target = new THREE.Vector3();
 
 init();
-animate();
+
+
+
 
 function init() {
-	var container, mesh;
+	var container, canvas, mesh;
 
-	container = document.getElementById( 'container' );
-  	var intro = document.getElementById( 'intro' );
+	container = document.body;
+  	// var intro = document.getElementById( 'intro' );
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
 
@@ -29,44 +31,91 @@ function init() {
 	context.fillStyle = 'rgb( 0, 0, 0 )';
 	context.fillRect( 0, 0, texture_placeholder.width, texture_placeholder.height );
 
-	var materials = [
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+		console.log( item, loaded, total );
+	};
 
-		loadTexture( 'assets/skyrt.jpg' ), 
-		loadTexture( 'assets/skylf.jpg' ),
-		loadTexture( 'assets/skyup.jpg' ),
-		loadTexture( 'assets/skydn.jpg' ),
-		loadTexture( 'assets/skybk.jpg' ),
-		loadTexture( 'assets/skyft.jpg' ),
 
+
+	manager.onLoad = function (){
+
+		mesh = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 300, 7, 7, 7 ), new THREE.MeshFaceMaterial( materials ) );
+		mesh.scale.x = - 1;
+		scene.add( mesh );
+
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	  	mid_x = window.innerWidth/2;
+	  	mid_y = window.innerHeight/2;
+	  	canvas = renderer.domElement;
+	  	container.appendChild( canvas);
+
+	  	composer = new WAGNER.Composer( renderer );
+		composer.setSize( window.innerWidth, window.innerHeight );
+		vignettePass = new WAGNER.VignettePass();
+
+	 	// gui = new dat.GUI();
+		// gui.add( vignettePass.params, 'amount' ).min(0).max(10);
+		// gui.add( vignettePass.params, 'falloff' ).min(0).max(10);
+		// gui.open();
+
+		vignettePass.params['amount'] = 0.8;
+		container.addEventListener( 'mousedown', onDocumentMouseDown, false );
+		container.addEventListener( 'mousemove', onDocumentMouseMove, false );
+		container.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+		container.addEventListener( 'touchstart', onDocumentTouchStart, false );
+		container.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+		window.addEventListener( 'resize', onWindowResize, false );
+		animate();
+	};
+
+	loader = document.querySelector(".loader");
+	bar = document.querySelector(".bar");
+	black = document.querySelector(".load").children[0];
+	main = document.querySelector(".main ").children[0];
+
+
+	TweenLite.set(black, {alpha:1});
+
+	tl = new TimelineLite();
+
+	tl.set(bar, {xPercent: -100, alpha:1})
+	.to(bar, 1, {xPercent: 0})
+	.set(bar, {yPercent:-100, height: "100%"})
+	.to(bar, 0.5, {yPercent: 0}, "invert")
+	.to(black, 0.5, {color:"#FFF"}, "invert")
+	.to(black, 2, {alpha:0}, "finish")
+	.to(loader, 1.5, {alpha:0}, 'finish')
+	.to(main, 2, {alpha:1 , ease:Power4.easeIn},'finish');
+
+	var assets = [
+		'assets/skyrt.jpg',
+		'assets/skylf.jpg',
+		'assets/skyup.jpg',
+		'assets/skydn.jpg',
+		'assets/skybk.jpg',
+		'assets/skyft.jpg'
 	];
 
-	mesh = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 300, 7, 7, 7 ), new THREE.MeshFaceMaterial( materials ) );
-	mesh.scale.x = - 1;
-	scene.add( mesh );
+	var materials = [];
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-  	mid_x = window.innerWidth/2;
-  	mid_y = window.innerHeight/2;
-  	container.appendChild( renderer.domElement );
-
-  	composer = new WAGNER.Composer( renderer );
-	composer.setSize( window.innerWidth, window.innerHeight );
-	vignettePass = new WAGNER.VignettePass();
-
- 	// gui = new dat.GUI();
-	// gui.add( vignettePass.params, 'amount' ).min(0).max(10);
-	// gui.add( vignettePass.params, 'falloff' ).min(0).max(10);
-	// gui.open();
-
-	intro.addEventListener( 'mousedown', onDocumentMouseDown, false );
-	intro.addEventListener( 'mousemove', onDocumentMouseMove, false );
-	intro.addEventListener( 'mouseup', onDocumentMouseUp, false );
-
-	intro.addEventListener( 'touchstart', onDocumentTouchStart, false );
-	intro.addEventListener( 'touchmove', onDocumentTouchMove, false );
-
-	window.addEventListener( 'resize', onWindowResize, false );
+	var closure = function (texture,material) {
+		return function(image){
+			texture.image=image;
+			texture.needsUpdate=true;
+		}
+	}
+	
+	for ( i = 0 ; i < assets.length ; i++){
+		texture = new THREE.Texture( texture_placeholder );
+		material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+		loader = new THREE.ImageLoader(manager);
+		materials.push(material);
+		loader.load( assets[i], closure(texture,material));
+	}
 }
 
 function onWindowResize() {
@@ -78,18 +127,9 @@ function onWindowResize() {
 	mid_y = window.innerHeight/2;
 }
 
-function loadTexture( path ) {
-
+function dummyTexture( ) {
 	var texture = new THREE.Texture( texture_placeholder );
 	var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
-
-	var image = new Image();
-	image.onload = function () {
-		texture.image = this;
-		texture.needsUpdate = true;
-	};
-	image.src = path;
-
 	return material;
 }
 
